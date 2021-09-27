6e925f88a01ac966c34e63fd602ddcb5b8458885
WendellLima <43830716+WendellLima@users.noreply.github.com>
2021-09-27 19:57:16 -0300

    melhorando menu

--- ArvoreBinaria/src/ArvoreBinariaBusca/main/Main.java
+++ ArvoreBinaria/src/ArvoreBinariaBusca/main/Main.java
@@ -3,8 +3,39 @@ package ArvoreBinariaBusca.main;
 import java.util.Scanner;
 
 public class Main {
+	
+
+	
+    public static void menu(){
+    	System.out.println("\n");
+    	System.out.println("\n");
+    	System.out.println("\n");
+		System.out.print("\n***********************************");
+		System.out.print("\n ----   Entre com a opcao:");
+		System.out.print("\n ----1: Imprimir Raiz e nós");
+		System.out.print("\n ----2: Imprimir nós folhas");
+		System.out.print("\n ----3: Pesquisar resultados de um nó");
+		System.out.print("\n ----4: Imprimir grau dos nós");
+		System.out.print("\n ----5: Imprimir altura dos nós");
+		System.out.print("\n ----6: Imprimir profundidade dos nós");
+		System.out.print("\n ----7: Imprimir níveis dos nós");
+		System.out.print("\n ----8: Imprimir subárvores");
+		System.out.print("\n ----9: Quantidade de nós");
+		System.out.print("\n ----10: Inserir um nó");
+		System.out.print("\n ----11: Remover um nó");
+		System.out.print("\n ----0: Sair do programa");
+		System.out.print("\n***********************************");
+		System.out.print("\n-> ");	
+		System.out.print("Informe sua opção de escolha:");
+		System.out.print("\n-> ");
+
+    }
+	
+	
 
 	public static void main(String[] args) {
+		
+
 		
 	Scanner entrada = new Scanner(System.in);
 	Scanner informeNo = new Scanner(System.in);
@@ -32,31 +63,19 @@ public class Main {
 
 
 		  
-		  
-			System.out.print("\n***********************************");
-			System.out.print("\n ----   Entre com a opcao:");
-			System.out.print("\n ----1: Imprimir Raiz e nós");
-			System.out.print("\n ----2: Imprimir nós folhas");
-			System.out.print("\n ----3: Pesquisar resultados de um nó");
-			System.out.print("\n ----4: Imprimir grau dos nós");
-			System.out.print("\n ----5: Imprimir altura dos nós");
-			System.out.print("\n ----6: Imprimir profundidade dos nós");
-			System.out.print("\n ----7: Imprimir níveis dos nós");
-			System.out.print("\n ----8: Imprimir subárvores");
-			System.out.print("\n ----9: Quantidade de nós");
-			System.out.print("\n ----10: Inserir um nó");
-			System.out.print("\n ----11: Remover um nó");
-			System.out.print("\n ----12: Sair do programa");
-			System.out.print("\n***********************************");
-			System.out.print("\n-> ");	
-			System.out.print("Informe sua opção de escolha:");
-			
-			
-		  int opcao = entrada.nextInt();
-		  switch (opcao) {
-		  
-		     case 1:
-		    	 
+	 int opcao;
+	do{	
+		
+
+		
+		
+		
+		
+         menu();
+         opcao = entrada.nextInt();
+         
+         switch(opcao){
+         case 1:
 		        System.out.println("A raiz da árvore é: " + ab.getArvoreBinaria().valor);
 		        System.out.print("\n");
 		        System.out.print("Segue relatório de nós: ");
@@ -70,111 +89,109 @@ public class Main {
 		   		System.out.print("\n");
 		   		System.out.print("PosOrdem" + "\n");
 		   		ab.PosOrdem(ab.getArvoreBinaria());
+             break;
+             
+         case 2:
+	    	 System.out.print("Os nós folhas são: " + "\n");
+	    	 ab.Nosfolhas(ab.getArvoreBinaria());
+             break;
+             
+         case 3:
+	    	 System.out.print("\n Informe o nó (inteiro) -> ");
+	    	 System.out.print("\n");
+	    	 int n = informeNo.nextInt();
+	    	
+	    	ab.verificarGrau(n);
+	    	ab.verificaAltura(n);
+	    	ab.verificaProfundidade(n);
+	    	ab.verificaNivel(n);
+             break;
+             
+         case 4:
+	    	   System.out.println("Segue relatório de grau por nó: ");
+	    	   ab.verificarAllgraus(ab.getArvoreBinaria());
+             break;
+             
+         case 5:
+        	 System.out.println("Segue relatório de altura por nó: ");
+		       ab.verificarAllAltura(ab.getArvoreBinaria());
+       	  
+       	  	break;
+       	  	
+         case 6:
+        	 System.out.println("Segue relatório de profundidade por nó: ");
+		       ab.verificarAllProfundidade(ab.getArvoreBinaria());
+          	  
+          	  break;
+          	  
+          	  
+         case 7:
+        	  System.out.println("Segue relatório de nível por nó: ");
+		       ab.verificarAllNivel(ab.getArvoreBinaria());
+          	  
+          	  break;
+          	  
+          	  
+         case 8:
+        	 System.out.println("Segue subárvores");
+		       ab.mostrar();
+          	  
+          	  break;
+         
+         case 9:
+        	 System.out.print("Quantidade de Nós: " + ab.contarNos(ab.getArvoreBinaria()));
+          	  
+          	  break;
+          	  
+         case 10:
+        	 System.out.print("\n Informe o nó para inserir -> ");
+	    	 System.out.print("\n");
+	    	 int s = informeNo.nextInt();
+	    	 
+	    	 
+	    	 System.out.print("\n A árvore antes de inserir é assim: ");  
+	    	 ab.mostrar();
+	    	 
+	    	 ab.adicionaNo(new No(s), ab.getArvoreBinaria());
+	    	 
+	    	 System.out.print("\n A árvore depois de inserir é assim: ");		    	 
+	    	 ab.mostrar();
+          	  
+          	  break;
+          	  
+         case 11:
 
-		       
-		       
-		       
-		       
-		       
-		       break;
-		     case 2:
-		    	 
-		    	 System.out.print("Os nós folhas são: " + "\n");
-		    	 ab.Nosfolhas(ab.getArvoreBinaria());
-		    		
-		       break;
-		     case 3:
-		    	 
-		    	 System.out.print("\n Informe o nó (inteiro) -> ");
-		    	 System.out.print("\n");
-		    	 int n = informeNo.nextInt();
-		    	
-		    	ab.verificarGrau(n);
-		    	ab.verificaAltura(n);
-		    	ab.verificaProfundidade(n);
-		    	ab.verificaNivel(n);
-		    		
-		       break;
-		     case 4:
-		    	 
-		    	   System.out.println("Segue relatório de grau por nó: ");
-		    	   ab.verificarAllgraus(ab.getArvoreBinaria());
-		    	   
-		    	   break;
-		     case 5:
-		    	 
-			       System.out.println("Segue relatório de altura por nó: ");
-			       ab.verificarAllAltura(ab.getArvoreBinaria());
-			       
-			       break;
-		     case 6:
-		    	 
-			       System.out.println("Segue relatório de profundidade por nó: ");
-			       ab.verificarAllProfundidade(ab.getArvoreBinaria());
-			       
-			       break;
-		     case 7:
-		    	 
-			       System.out.println("Segue relatório de nível por nó: ");
-			       ab.verificarAllNivel(ab.getArvoreBinaria());
-			       
-			       break;
-		     case 8:
-		    	 
-			       System.out.println("Segue subárvores");
-			       ab.mostrar();
-			       
-			       break;
+	    	 System.out.print("\n Informe o nó para remover -> ");
+	    	 System.out.print("\n");
+	    	 int r = informeNo.nextInt();
+	    	 
+	    	 
+	    	 System.out.print("\n A árvore antes de remover é assim: ");  
+	    	 ab.mostrar();
+	    	 
+	    	 ab.remover(r);
+	    	 
+	    	 System.out.print("\n A árvore depois de remover é assim: ");		    	 
+	    	 ab.mostrar();
+          	  
+          	  break;
+          	  
+	  
+         
+         default:
+             System.out.println("Programa encerrado.");
+         }
+     } while(opcao != 0);
 
-		     case 9:
-		    	 
 
-			       System.out.print("Quantidade de Nós: " + ab.contarNos(ab.getArvoreBinaria()));
-			       
-			       break;    
-			       
-			       
-		     case 10:
-		    	 
-
-		    	 System.out.print("\n Informe o nó para inserir -> ");
-		    	 System.out.print("\n");
-		    	 int s = informeNo.nextInt();
-		    	 
-		    	 
-		    	 System.out.print("\n A árvore antes de inserir é assim: ");  
-		    	 ab.mostrar();
-		    	 
-		    	 ab.adicionaNo(new No(s), ab.getArvoreBinaria());
-		    	 
-		    	 System.out.print("\n A árvore depois de inserir é assim: ");		    	 
-		    	 ab.mostrar();
-			       
-			       break;     
-			       
-			       
-		     case 11:
-		    	 
-
-		    	 System.out.print("\n Informe o nó para remover -> ");
-		    	 System.out.print("\n");
-		    	 int r = informeNo.nextInt();
-		    	 
-		    	 
-		    	 System.out.print("\n A árvore antes de remover é assim: ");  
-		    	 ab.mostrar();
-		    	 
-		    	 ab.remover(r);
-		    	 
-		    	 System.out.print("\n A árvore depois de remover é assim: ");		    	 
-		    	 ab.mostrar();
-			       
-			       break;      
-			       
-			       
-		     default:
-		       System.out.println("Número inválido");
-		  }
+			
+	 
+	 
+	 
+	 
+	 
+			
+		 
 		
 	
 
